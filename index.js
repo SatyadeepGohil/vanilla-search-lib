f2be474d3545846d38649c719f8ce07c75d61a78
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2025-06-05 21:05:32 +0530

    Experimental restructure from if structure to switch sturcture.

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 class Search {
     constructor(data) {
-        this.data = data || [];
+        this.searchData = data || [] || {};
+        this.originalData = this.searchData;
     }
 
     search(query, options = {}) {
-        const {mode = 'exact', caseSensitive = false } = options;
-
-        if (mode != 'exact') {
-            throw new Error('Only exact mode is implement for now');
+        const {mode = 'exact', caseSensitive = false, normalzie = false } = options;
+        
+        if (mode !== 'exact') {
+            throw new Error('Only exact mode is implement for now')
         }
+        
+        switch(mode){
+            case 'exact':
 
-        if (!caseSensitive) {
-            query = query.toLowerCase();
-            return this.data.filter(item => item.toLowerCase() === query);
-        } else {
-            return this.data.filter(item => item === query);
+                if (!caseSensitive) {
+                    query = query.toLowerCase();
+                    return this.searchData.filter(item => item.toLowerCase() === query);
+                } else {
+                    return this.searchData.filter(item => item === query);
+                }
         }
+    }
+
+    normalzie(query) {
+
     }
 }
 
 
-const searchInstace = new Search(['apple', 'banana', 'Apple', 'orange']);
+const searchInstance = new Search(['apple', 'banana', 'Apple', 'orange', 'orang3']);
 
-console.log(searchInstace.search('apple', { caseSensitive: true }));
+console.log(searchInstance.search('banana', { caseSensitive: true }));
 
-console.log(searchInstace.search('Apple'));
+console.log(searchInstance.search('apple', { mode: 'not-exact'}));
+
+console.log(searchInstance.search('orange'));
