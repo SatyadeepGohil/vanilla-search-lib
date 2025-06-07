e774d661deb18b63289428a8e90e6fce120a6b10
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2025-06-07 21:39:04 +0530

    ## Exact Mode feature completed - Added normalization of data when needed - The originalData now gets assinged directly the inputed data - resetData function added for multiple searches in short time. - Tested the options with simple console.log for now and it works.

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
 class Search {
     constructor(data) {
-        this.searchData = data || [] || {};
-        this.originalData = this.searchData;
+        this.originalData = data || [];
+        this.searchData = [...this.originalData];
     }
 
     search(query, options = {}) {
-        const {mode = 'exact', caseSensitive = false, normalzie = false } = options;
-        
-        if (mode !== 'exact') {
-            throw new Error('Only exact mode is implement for now')
+        const {mode = 'exact', caseSensitive = false, isNormalize = false} = options;
+        let searchQueryNormalize;
+
+        if (caseSensitive && isNormalize) {
+            throw new Error("caseSensitive and isNormalize can't be used at once. it gives incorrect search result, So it's prohibited to use.")
         }
-        
+
+        if (isNormalize) {
+            searchQueryNormalize = this.normalize(query);
+        }
         switch(mode){
             case 'exact':
 
-                if (!caseSensitive) {
-                    query = query.toLowerCase();
-                    return this.searchData.filter(item => item.toLowerCase() === query);
-                } else {
+                if (caseSensitive) {
                     return this.searchData.filter(item => item === query);
                 }
+                
+                if (isNormalize){
+                    return this.searchData.filter(item => item.toLowerCase() === searchQueryNormalize);
+                }
+
+                if (!caseSensitive && !isNormalize) {
+                    return this.searchData.filter(item => item === query)
+                }
+                
+                break;
+            default:
+                throw new Error('No modes matched with existing ones.');
         }
+
     }
 
-    normalzie(query) {
+    normalize(query) {
 
+            if (typeof query === 'string') {
+                query = query.toLowerCase();
+                for (let i = 0; i < this.searchData.length; i++) {
+                    this.searchData[i] = this.searchData[i].toLowerCase();
+                }
+                this.resetData();
+                return query;
+            } else {
+                throw new Error('Not a string.')
+            }
+    }
+
+    resetData () {
+        this.searchData = [...this.originalData];
     }
 }
 
 
-const searchInstance = new Search(['apple', 'banana', 'Apple', 'orange', 'orang3']);
+const searchInstance = new Search(['apple', 'banana', 'Banana', 'Apple', 'orange', 'orang3']);
 
 console.log(searchInstance.search('banana', { caseSensitive: true }));
 
-console.log(searchInstance.search('apple', { mode: 'not-exact'}));
+console.log(searchInstance.search('Apple', {caseSensitive: true}));
+
+console.log(searchInstance.search('Apple', { isNormalize: true}));
+
+console.log(searchInstance.search('Banana', { isNormalize: true }));
+
+/* console.log(searchInstance.search('apple', { mode: 'not-exact'})); */
 
 console.log(searchInstance.search('orange'));
